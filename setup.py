fef7d709bba75fbcb421728ab7f2578bbe53f807
RogerQi <rizhaoq2@illinois.edu>
2024-06-02 14:00:19 -0700

    [FIX] add vocab file to package data for installation w/o -e

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="maskclip_onnx",
     version="1.0",
-    install_requires=[],
+    install_requires=["torch"],
+    include_package_data=True,
+    package_data={'maskclip_onnx': ['bpe_simple_vocab_16e6.txt.gz']},
     packages=find_packages(exclude="notebooks"),
     extras_require={
         "all": ["matplotlib", "pycocotools", "opencv-python", "onnx", "onnxruntime"],
